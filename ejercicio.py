a69438ff0cf7e10a5b6ff6b4d193d4738519f89f
developer.VEN <rivasrodriguezwillber.369@gmail.com>
2022-10-20 14:24:07 -0400

    [ADD] exercise one

---
+++ ejercicio.py
@@ -0,0 +1,33 @@
+#Developer Willber
+#thecnical Test - multilpe number of three and five 
+
+blackfin = 0 
+
+while blackfin < 100:
+    print(blackfin)
+    if blackfin % 3 == 0:
+        print("VIM")
+        if blackfin % 5 == 0:
+            print("CLE")
+            if blackfin % 3 == 0:
+                if blackfin % 5 == 0:
+                 print("VINCLE")
+
+    blackfin += 1
+    
+
+    
+          
+
+
+
+
+
+
+
+
+
+  
+
+
+
